ed748ed28dc50385017568528b7e934cc19c89f0
Praizleye <Praiseleye.pl@gmail.com>
2023-03-02 23:52:57 -0500

    Pushing frontend folder of react blog. interface still in build but we getting there slowly

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+// !importting components
+import  Nav from "./components/Nav/Nav"
+import SideBar from "./components/Nav/SideBar";
+
+function App() {
+ 
+
+  return (
+    <div>
+      {/* <Nav /> */}
+      <SideBar />
+   </div>
+  )
+}
+
+export default App
